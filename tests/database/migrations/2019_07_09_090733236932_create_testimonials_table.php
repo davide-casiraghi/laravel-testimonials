a526673090aa2da73f8c37a670aefec5ee79d094
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-07-09 11:43:57 +0200

    fixed test migrations

---
+++ tests/database/migrations/2019_07_09_090733236932_create_testimonials_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestimonialsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->integer('testimonials_group')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('image_file_name')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+}
